29390c0e1bbbf295ca018fb9431e7d772d76aaba
hernandessilva11-hue <hernandes.silva11@icloud.com>
2025-11-30 17:35:34 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,19 @@
+import pg from "pg";
+const { Pool } = pg;
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  console.error("❌ ERRO: DATABASE_URL não encontrada nas variáveis de ambiente!");
+  process.exit(1);
+}
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
+
+export default pool;
 
